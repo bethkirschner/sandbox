82c828873490a2e7ac09f81247fcac612c9a6949
Beth Kirschner <bkirschn@umich.edu>
2016-01-19 15:45:45 -0500

    updated remove-site-users.pl script

--- ctools/remove-site-users.pl
+++ ctools/remove-site-users.pl
@@ -9,9 +9,26 @@
 # 
 
 use strict;
+my $CREATE_ARCHIVE_SQL = qq{
+drop table ARCHIVE_SAKAI_SITE_USER;
+drop table ARCHIVE_SAKAI_REALM_RL_GR;
+create table ARCHIVE_SAKAI_SITE_USER (
+    SITE_ID              VARCHAR2(99) NOT NULL,
+    USER_ID              VARCHAR2(99) NOT NULL,
+    PERMISSION           INTEGER NOT NULL );
+create table ARCHIVE_SAKAI_REALM_RL_GR (
+    REALM_KEY            INTEGER NOT NULL,
+    USER_ID              VARCHAR2(99) NOT NULL,
+    ROLE_KEY             INTEGER NOT NULL,
+    ACTIVE               CHAR(1) NULL CHECK (ACTIVE IN (1, 0)),
+    PROVIDED             CHAR(1) NULL CHECK (PROVIDED IN (1, 0)) );
+};
+
+my $ARCHIVE_USER_SQL = "insert into ARCHIVE_SAKAI_SITE_USER (select * from SAKAI_SITE_USER where site_id = 'SITE-ID');\n";
+my $ARCHIVE_REALM_SQL = "insert into ARCHIVE_SAKAI_REALM_RL_GR (select * from SAKAI_REALM_RL_GR where realm_key in (select realm_key from sakai_realm where realm_id like '/site/SITE-ID%'));\n";
 
 my $USER_SQL = "delete from SAKAI_SITE_USER where site_id = 'SITE-ID';\n";
-my $REALM_SQL = "delete from sakai_realm_rl_gr where realm_key in (select realm_key from sakai_realm where realm_id like '/site/SITE-ID%');\n";
+my $REALM_SQL = "delete from SAKAI_REALM_RL_GR where realm_key in (select realm_key from sakai_realm where realm_id like '/site/SITE-ID%');\n";
 my $COMMIT_SQL = "commit;\n";
 
 main:
@@ -31,12 +48,23 @@ main:
    close INFILE;
 
    open OUTFILE, "> $outFile" or die "$!";
+   print OUTFILE $CREATE_ARCHIVE_SQL;
+   
    SITE: foreach $_ (@sites)
    {
        chomp;
        next SITE if ( !$_ );
+       my $sql;
        
-       my $sql = $USER_SQL;
+       $sql = $ARCHIVE_USER_SQL;
+       $sql =~ s/SITE-ID/$_/;
+       print OUTFILE $sql;
+       
+       $sql = $USER_SQL;
+       $sql =~ s/SITE-ID/$_/;
+       print OUTFILE $sql;
+       
+       $sql = $ARCHIVE_REALM_SQL;
        $sql =~ s/SITE-ID/$_/;
        print OUTFILE $sql;
        
